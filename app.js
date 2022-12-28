b29e5ab62424afd75ac11bd87dc4717956bd393b
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2022-12-28 17:18:01 -0500

    Create responsive 4x4 grid of gray divs

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("testing, testing, one two three");
+const createCell = () => {
+    const cell = document.createElement("div");
+    cell.classList.add("cell");
+    return cell;
+};
+
+const cells = Array.from({length: 16}, createCell);
+
+const cellGrid = document.querySelector("main.cell-grid");
+
+cells.forEach(cell => cellGrid.appendChild(cell));
