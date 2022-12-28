0a515472f13ea172bfc40b310ca37b00631600f1
Brandon C. Irizarry <brandon.irizarry@gmail.com>
2022-12-28 18:30:53 -0500

    Support varying grid sizes based on user input
    I also make sure to sanitize the input as it comes in, since I'm using
    'prompt' for now.

--- app.js
+++ app.js
@@ -7,18 +7,24 @@ const createCell = () => {
 const constructGridInternal = dimension => {
     const cellGridArea = dimension * dimension;
     const cells = Array.from({length: cellGridArea}, createCell);
-    const cellGrid = document.querySelector("main.cell-grid");
-    cellGrid.style.gridTemplateColumns = `repeat(${dimension}, 1fr`;
-    cells.forEach(cell => cellGrid.appendChild(cell));
-};
 
-constructGridInternal(16);
+    return cells;
+};
 
 const gridDimensionButton = document.querySelector("button.prompt-dimension");
 gridDimensionButton.addEventListener("click", constructGrid);
 
-function constructGrid () {
-    const dimension = prompt("Grid dimension:") ?? 16;
+const cellGrid = document.querySelector("main.cell-grid");
 
-    console.log(dimension);
+function constructGrid () {
+    let dimension = parseInt(prompt("Grid dimension:"));
+
+    if (isNaN(dimension)) {
+	dimension = 16;
+    }
+
+    const cells = constructGridInternal(dimension);
+    cellGrid.style.gridTemplateColumns = `repeat(${dimension}, 1fr`;
+    [...document.querySelectorAll(".cell")].map(cell => cell.remove());
+    cells.forEach(cell => cellGrid.appendChild(cell));
 }
